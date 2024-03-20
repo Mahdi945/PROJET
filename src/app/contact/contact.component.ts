e71fffc6339152e73726f280342ab1fac2355d70
Mahdi Bey <mahdibeyy@gmail.com>
2024-03-21 00:03:56 +0100

    fourth commit

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -33,7 +33,7 @@ export class ContactComponent {
         descriptionPanne: this.descriptionPanne
     };
 
-    this.http.post<any>('https://node-3-n4qo.onrender.com/submitForm/ajout', formData)
+    this.http.post<any>('https://node-3-n4qo.onrender.com', formData)
       .subscribe(
         (response) => {
           console.log('Réponse du serveur:', response);
